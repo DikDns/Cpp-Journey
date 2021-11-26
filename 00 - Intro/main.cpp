2b6509cb85300bf32ed58986b30f919b5ee8025d
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2021-11-26 16:39:47 +0700

    Introducing Cpp 🟢

---
+++ 00 - Intro/main.cpp
@@ -0,0 +1,21 @@
+/*
+    Menjelaskan Apa Maksud dari Instruksi dibawah ini
+    Comment dan Kosong tidak akan di eksekusi
+*/
+
+// untuk copy paste file library iostream ke sini
+#include <iostream>
+
+// pintu masuk
+int main() {
+
+    // rentetan instruksi console out
+    std::cout << "Hello World!" << std::endl;
+
+    std::cin.get();
+    // karena si int harus dibalikin sesuatu
+    return 0;
+}
+
+// Lalu cara compile nya?
+// Perbedaan dengan sebelum di cin.get()
