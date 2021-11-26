d86d0714f5cf1cc33cb21420fb6c9b5801d102cd
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2021-11-26 16:40:37 +0700

    Compiling 🟢

---
+++ 01 - Compiling/main.cpp
@@ -0,0 +1,23 @@
+/*
+    Menjelaskan Inti nya saja!
+
+    Preprocessing -> Compiling -> Linking
+
+    Preprocess File:
+        g++ -E main.cpp > main.p
+
+    menjelaskan tanbda pagar hashtag
+
+    Compiling:
+    Berisi Obj file or Machine Language
+        objdump -d .\main.o 
+
+
+*/
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    std::cin.get();
+    return 0;
+#include  "kurung.h"
