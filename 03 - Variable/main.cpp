0b42914e854143d1d31b96451e07f9bb64ffe73e
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2021-11-26 16:42:03 +0700

    Variable Explanation 🟢

---
+++ 03 - Variable/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Konsep Variabel
+    kebalik
+    x = 10
+    dari kanan ke kiri
+*/
+#include <iostream>
+
+int main(){
+    // mendeklarasikan integer: bilangan bulat
+    int x = 10;
+    std::cout << x << " <- adalah isi variabel x\n";
+    std::cout << "X = " << x << std::endl;
+    std::cin.get();
+    return 0;
+}
